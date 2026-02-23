5e41c1fd35c8610f44a8c99c3cf5c8cf62a9f3ca
Chinedu Ejibendu <chinextworld@gmail.com>
2026-02-23 07:41:02 +0000

    something just happened to the layout and I don't know when. currently t

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -13,17 +13,17 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
   };
 
   return (
-    <div className="flex min-h-screen w-full bg-background">
+    <div className="flex h-screen w-full overflow-hidden bg-background">
       <MainSidebar />
       <SecondarySidebar isOpen={isSecondarySidebarOpen} />
       <div
         className={cn(
-          'flex flex-1 flex-col transition-all duration-300 ease-in-out',
+          'flex flex-1 flex-col min-w-0 transition-all duration-300 ease-in-out',
           isSecondarySidebarOpen ? 'sm:pl-[320px]' : 'sm:pl-[64px]'
         )}
       >
         <Header onToggleSidebar={toggleSecondarySidebar} />
-        <main className="flex-1 overflow-y-auto p-4 sm:p-6">{children}</main>
+        <main className="flex-1 overflow-auto p-4 sm:p-6">{children}</main>
       </div>
     </div>
   );
